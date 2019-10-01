e3da47e8053b806e22a215d2974c59557f8d4552
unknown <dungmt10697@gmail.com>
2019-10-01 16:39:40 +0700

    fix

--- learn-react/src/booking/BasicCalendar.js
+++ learn-react/src/booking/BasicCalendar.js
@@ -1,30 +1,98 @@
-import React from 'react'
-import { Calendar, Views } from 'react-big-calendar'
-import events from '../events'
-import * as dates from 'react-big-calendar/lib/utils/dates'
+import React, {Component} from 'react';
+import {Calendar, momentLocalizer, Views} from 'react-big-calendar'
+import moment from 'moment';
+import 'moment/locale/vi';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import events from "../events";
 
-let allViews = Object.keys(Views).map(k => Views[k]);
+const localizer = momentLocalizer(moment);
+const DragAndDropCalendar = withDragAndDrop(Calendar);
+export default class Schedule extends Component {
+    constructor(props) {
+        super(props);
 
-const ColoredDateCellWrapper = ({ children }) =>
-    React.cloneElement(React.Children.only(children), {
-        style: {
-            backgroundColor: 'lightblue',
-        },
-    });
+        this.moveEvent = this.moveEvent.bind(this);
+    }
 
-let Basic = ({ localizer }) => (
-    <Calendar
-        events={events}
-        views={allViews}
-        step={60}
-        showMultiDayTimes
-        max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
-        defaultDate={new Date(2015, 3, 1)}
-        components={{
-            timeSlotWrapper: ColoredDateCellWrapper,
-        }}
-        localizer={localizer}
-    />
-);
+    state = {
+        events: events,
+        culture: 'vi'
+    };
 
-export default Basic
+    handleSelect = ({start, end}) => {
+        const title = window.prompt('New Event name');
+        if (title)
+            this.setState({
+                events: [
+                    ...this.state.events,
+                    {
+                        start,
+                        end,
+                        title,
+                    },
+                ],
+            })
+    };
+
+    moveEvent({event, start, end, isAllDay: droppedOnAllDaySlot}) {
+        const {events} = this.state;
+
+        const idx = events.indexOf(event)
+        let allDay = event.allDay;
+
+        if (!event.allDay && droppedOnAllDaySlot) {
+            allDay = true
+        } else if (event.allDay && !droppedOnAllDaySlot) {
+            allDay = false
+        }
+
+        const updatedEvent = {...event, start, end, allDay};
+
+        const nextEvents = [...events];
+        nextEvents.splice(idx, 1, updatedEvent);
+
+        this.setState({
+            events: nextEvents,
+        })
+
+        // alert(`${event.title} was dropped onto ${updatedEvent.start}`)
+    }
+
+    resizeEvent = ({event, start, end}) => {
+        const {events} = this.state;
+
+        const nextEvents = events.map(existingEvent => {
+            return existingEvent.id === event.id
+                ? {...existingEvent, start, end}
+                : existingEvent
+        });
+
+        this.setState({
+            events: nextEvents,
+        })
+
+        //alert(`${event.title} was resized to ${start}-${end}`)
+    }
+
+    render() {
+        return (
+            <div className='container' style={{height: '100vh'}}>
+                <DragAndDropCalendar
+                    selectable
+                    localizer={localizer}
+                    events={this.state.events}
+                    onEventDrop={this.moveEvent}
+                    resizable
+                    onEventResize={this.resizeEvent}
+                    culture={this.state.culture}
+                    defaultView={Views.WEEK}
+                    scrollToTime={new Date(1970, 1, 1, 6)}
+                    onSelectEvent={event => alert(event.title)}
+                    onSelectSlot={this.handleSelect}
+                />
+            </div>
+
+        )
+    }
+}
--- learn-react/src/booking/booking.js
+++ learn-react/src/booking/booking.js
@@ -1,86 +1,29 @@
-// import React from 'react';
-// import Calendar from "react-calendar";
-// // import {Calendar as BigCalendar} from 'react-big-calendar';
-// // import moment from 'moment';
-// import Header from '../Components/Header';
-// import './booking.css';
-// import Basic from "./BasicCalendar";
-//
-// class Booking extends React.Component {
-//     render() {
-//         // const localizer = BigCalendar.momentLocalizer(moment);
-//         // const MyCalendar = props => (
-//         //     <div>
-//         //         <BigCalendar
-//         //             localizer={localizer}
-//         //             // events={myEventsList}
-//         //             startAccessor="start"
-//         //             endAccessor="end"
-//         //         />
-//         //     </div>
-//         // );
-//         return (
-//             <div className="col-md-12" id="table">
-//                 <div className="row">
-//                     <Header/>
-//                 </div>
-//                 <br/><br/><br/>
-//                 <div className="row">
-//                     <div className="col-md-2">
-//                         <Calendar/>
-//                         <Basic/>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-//
-// export default Booking
+import React from 'react';
+import Calendar from "react-calendar";
+import Header from '../Components/Header';
+import './booking.css';
+import Schedule from "./BasicCalendar";
+import DndCalendar from "./DndCalendar";
 
-import React, { Component } from 'react';
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
-import moment from 'moment';
-import 'moment/locale/vi';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-const localizer = momentLocalizer(moment);
-
-export default class Schedule extends Component {
-    state = {
-        events: [],
-        culture: 'vi'
-    }
-
-    handleSelect = ({ start, end }) => {
-        const title = window.prompt('New Event name')
-        if (title)
-            this.setState({
-                events: [
-                    ...this.state.events,
-                    {
-                        start,
-                        end,
-                        title,
-                    },
-                ],
-            })
-    }
+class Booking extends React.Component {
     render() {
         return (
-            <div className='container' style={{height: '100vh'}}>
-                <Calendar
-                    selectable
-                    localizer={localizer}
-                    events={this.state.events}
-                    culture={this.state.culture}
-                    defaultView={Views.WEEK}
-                    scrollToTime={new Date(1970, 1, 1, 6)}
-                    onSelectEvent={event => alert(event.title)}
-                    onSelectSlot={this.handleSelect}
-                />
+            <div className="col-md-12" id="table">
+                <div className="row">
+                    <Header/>
+                </div>
+                <br/><br/><br/>
+                <div className="row">
+                    <div className="col-md-2">
+                        <Calendar/>
+                    </div>
+                    <div className="col-md-10">
+                        <DndCalendar/>
+                    </div>
+                </div>
             </div>
-
         )
     }
 }
+
+export default Booking
